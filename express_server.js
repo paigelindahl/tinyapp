62772eed8899898b0e5e3619aa51f947ad02e041
paigelindahl <paigemarlindahl@gmail.com>
2020-09-30 23:45:22 +0000

    altered registration to catch missing info and already existing users

--- express_server.js
+++ express_server.js
@@ -14,10 +14,47 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
 
 function generateRandomString() {
   return Math.random().toString(20).substr(2, 6);
 };
+
+app.post("/register", (req, res) => {
+  if (req.body.email === '') {
+    res.status(400).send('Please enter an email/password');
+  } 
+  
+  for (let userId in users) {
+    const user = users[userId];
+    if (user.email === req.body.email) {
+      res.status(400).send('Email already registered');
+    }
+  };
+
+  
+  let newUserId = generateRandomString();
+
+  users[newUserId] = {
+    id: newUserId,
+    email: req.body.email,
+    password: req.body.password
+  };
+  res.cookie('user_id', newUserId);
+  res.redirect("/urls");
+});
 
 app.post("/urls", (req, res) => {
   console.log(req.body); 
@@ -38,7 +75,7 @@ app.post("/urls/:id", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-  res.cookie('username',req.body.username);
+  // res.cookie('username',req.body.username);
   res.redirect('/urls');
 });
 
@@ -62,8 +99,8 @@ app.get("/hello", (req, res) => {
 
 app.get("/urls/new", (req, res) => {
   const templateVars = {
-    username: req.cookies["username"]
-  }
+    user: users[req.cookies["user_id"]]
+  };
   res.render("urls_new", templateVars);
 });
 
@@ -71,7 +108,7 @@ app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
     shortURL: req.params.shortURL, 
     longURL: urlDatabase[req.params.shortURL], 
-    username: req.cookies["username"]
+    user : users[req.cookies["user_id"]]
   };
   res.render("urls_show", templateVars);
 });
@@ -81,10 +118,18 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
+app.get("/register", (req, res) => {
+  const templateVars = {
+    user: users[req.cookies["user_id"]]
+  };
+  res.render("registration", templateVars);
+})
+
 app.get('/urls', (req, res) => {
   const templateVars = {
     urls: urlDatabase,
-  username: req.cookies["username"] };
+    user: users[req.cookies["user_id"]]
+  };
   res.render('urls_index', templateVars);
 });
 
