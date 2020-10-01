f1d814a477ceacb869b8b5b7241afcf1d5f8f8c1
paigelindahl <paigemarlindahl@gmail.com>
2020-10-01 01:10:50 +0000

    created login post request that checked whether password and email were correct and created a cookie for userid

--- express_server.js
+++ express_server.js
@@ -32,6 +32,8 @@ function generateRandomString() {
   return Math.random().toString(20).substr(2, 6);
 };
 
+
+
 app.post("/register", (req, res) => {
   if (req.body.email === '' || req.body.password === '') {
     res.status(400).send('Please enter an email/password');
@@ -74,28 +76,21 @@ app.post("/urls/:id", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-  console.log('this is req.body', req.body);
-  // res.cookie('username',req.body.username);
-  res.redirect('/urls');
-});
+  for (let userId in users) {
+    const user = users[userId];
+    if (user.email === req.body.email && user.password === req.body.password) {
+      res.cookie('user_id', userId);
+      res.redirect('/urls');
+     } 
+    }
+    res.status(403).send('The email/password you provided is incorrect');
+  });
+
 
 app.post("/logout", (req, res) => {
-  res.clearCookie('username');
+  res.clearCookie('user_id');
   res.redirect("/urls");
 });
-
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
 
 app.get("/urls/new", (req, res) => {
   const templateVars = {
@@ -139,6 +134,18 @@ app.get('/urls', (req, res) => {
     user: users[req.cookies["user_id"]]
   };
   res.render('urls_index', templateVars);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.get('*', (req, res) => {
