d56920e6ca773fa9a8b4f08772788e5bc24c6fdd
paigelindahl <paigemarlindahl@gmail.com>
2020-10-02 02:46:23 +0000

    added chai mocha testing

--- express_server.js
+++ express_server.js
@@ -3,7 +3,7 @@ const app = express();
 const cookieSession = require('cookie-session');
 app.use(cookieSession({
   name: 'session', 
-  keys: ['key1', 'key']
+  keys: ['key1', 'key2']
 }));
 const PORT = 8080; 
 app.set("view engine", "ejs");
--- test/helpersTest.js
+++ test/helpersTest.js
@@ -1,6 +1,6 @@
 const { assert } = require('chai');
-
 const { getUserByEmail } = require('../helpers.js');
+// const assert = chai.assert;
 
 const testUsers = {
   "userRandomID": {
@@ -17,8 +17,24 @@ const testUsers = {
 
 describe('getUserByEmail', function() {
   it('should return a user with valid email', function() {
-    const user = getUserByEmail("user@example.com", users)
+    const user = getUserByEmail("user@example.com", testUsers)
     const expectedOutput = "userRandomID";
-    // Write your assert statement here
+    assert.equal(expectedOutput, user);
   });
+
+  it('should return false when an invalid email is provided', function() {
+    const user = getUserByEmail("testing@gmail.com", testUsers)
+    const expectedOutput = false;
+    assert.equal(expectedOutput, user);
+  });
+  it('should return false since it does not return an object', function() {
+    const user = getUserByEmail("user@example.com", testUsers)
+    const expectedOutput = false;
+    assert.isNotObject(expectedOutput, user);
+  })
+  it('should return return false when passed an empty email string', function() {
+    const user = getUserByEmail("", testUsers)
+    const expectedOutput = false;
+    assert.equal(expectedOutput, user);
+  })
 });
